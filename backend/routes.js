e5cc3e393a45b4b10f8130386b3cb88abba8152f
nimmala naresh <nageshnimmala48@gmail.com>
2024-08-05 20:45:55 +0530

    sample route configuration

---
+++ backend/routes.js
@@ -0,0 +1,13 @@
+// user creation http://{domain}: {port}/api/v1/user
+// get call http://{domain}: {port}/api/v1/user
+// for paricular user http://{domain}: {port}/api/v1/user/userId
+// put call  http://{domain}: {port}/api/v1/user/userId
+// delete call  http://{domain}: {port}/api/v1/user/userId
+// login http://{domaiin}:{port}/api/login
+
+
+// job
+// job creation  http://{domain}: {port}/api/v1/job
+// same all calls
+// based on role if you want to fetch add query
+// http://{domain}: {port}/api/v1?query= {role: "some roele", member: "userId"}
